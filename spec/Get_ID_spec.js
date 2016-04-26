5eb4e4a204a1eab9c1b3ec279dc4d38fb18ca53d
Apurva <apurva.khune@bighlf.io>
2016-04-26 22:52:14 +0530

    msg

--- spec/Get_ID_spec.js
+++ spec/Get_ID_spec.js
@@ -2,12 +2,13 @@
 * Created by Apurva on 31/3/16.
 */
 
-   var commonCfg = require("../Frisby_Inputs/CommonConfiguration_spec.js");
-   var cfg = require("../Frisby_Inputs/DataSquareInputs_spec.js");
+   //var commonCfg = require("../Frisby_Inputs/CommonConfiguration_spec.js");
+   //var cfg = require("../Frisby_Inputs/DataSquareInputs_spec.js");
    var frisby = require(commonCfg.dev_frisby_lib_location);
    
    frisby.create(cfg.datasquareServices.desc)
-  .get(cfg.datasquareServices.Get_ID.url)
-  .addHeader(commonCfg.authrizationKey,commonCfg.authrizationValue)
+  //.get(cfg.datasquareServices.Get_ID.url)
+  .get(https://dev-services.bighalf.io/DataSquareService/user/tfortesting16@gmail.com/datasquares/et745c60a8-b38f-4e9d-8d5d-26ac64a4d7ac)
+  //.addHeader(commonCfg.authrizationKey,commonCfg.authrizationValue)
   .expectStatus(200)
   .toss()
